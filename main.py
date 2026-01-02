60e942817f5620e344137cc9f58b599cac11dce6
desmond.chong <desmond.chong@beans.com.my>
2026-01-02 17:59:33 +0800

    feat: complete backend MVP with OpenAI and Firecrawl integration

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Form
 from PyPDF2 import PdfReader
 from firecrawl import Firecrawl
 from dotenv import load_dotenv
+from openai import OpenAI
 import os
+
+from pyexpat.errors import messages
+
 app = FastAPI()
 
 load_dotenv()
 FIRECRAWL_API_KEY = os.getenv("FIRECRAWL_API_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+client = OpenAI(api_key=OPENAI_API_KEY)
 
 firecrawl = Firecrawl(api_key=FIRECRAWL_API_KEY)
 @app.get("/")
@@ -14,15 +20,26 @@ async def home():
     return {"message": "Fit Signal is alive!"}
 
 @app.post("/analyze/")
-async def analyze(file: UploadFile):
+async def analyze(file: UploadFile = File(...), company_blog_url: str = Form(...), job_description_url: str = Form(...)):
     pdfReader = PdfReader(file.file)
+    resume = ""
+    company_blog = firecrawl.scrape(company_blog_url, formats=["markdown"])
+    job_description = firecrawl.scrape(job_description_url, formats=["markdown"])
+
     for page in pdfReader.pages:
-        print(page.extract_text())
-    return {"file": file.filename}
+        resume += page.extract_text()
+    system_prompt = "Act as a senior tech recruiter for a company. I will give you the job description you post and the blogs your company post that delimited by triple hashtag, Your goal is to rewrite the resume delimited by triple backtick to match the company desire candidates."
+    resume = f"```{resume}```"
+    company_info = f"###{company_blog.markdown + job_description.markdown}###"
 
-@app.get("/scrape/")
-async def scrape_url(url:str):
-    doc = firecrawl.scrape(url, formats=["markdown","html"])
-    print(doc)
-
-    return {"url": url}
+    response = client.chat.completions.create(
+        model="gpt-5-nano",
+        messages=[{
+            "role": "system",
+            "content": system_prompt,
+        }, {
+            "role": "user",
+            "content": resume + company_info,
+        }],
+    )
+    return {"file": response.choices[0].message.content}
